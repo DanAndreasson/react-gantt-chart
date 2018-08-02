47f4cd7fc557a027bb5ef97c0569e0c760410378
Dan Andreasson <dan@saits.se>
2018-08-02 16:21:51 +0200

    init

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,95 @@
-import React, { Component } from 'react'
+import React, { Component } from "react"
 
-import ExampleComponent from 'react-gantt-chart'
+import Gantt from "react-gantt-chart"
+
+class Row extends Component {
+  render() {
+    const { project, startsAt, endsAt, options } = this.props
+
+    console.log(options)
+
+    return (
+      <div className={"row"} style={options}>
+        {project.name}. {startsAt}. {endsAt}
+      </div>
+    )
+  }
+}
 
 export default class App extends Component {
-  render () {
+  state = {
+    startsAt: "2018-01-01",
+    endsAt: "2018-12-01",
+    name: "",
+    items: [
+      {
+        id: 1,
+        project: { name: "Doorling" },
+        startsAt: "2018-03-01",
+        endsAt: "2018-07-01",
+      },
+      {
+        id: 2,
+        project: { name: "SpiderAds 2.0" },
+        startsAt: "2018-01-01",
+        endsAt: "2018-01-31",
+      },
+    ],
+  }
+
+  addItem = item => this.setState({ items: [...this.state.items, item] })
+
+  handleChange = ({ target }) => this.setState({ [target.name]: target.value })
+
+  handleSubmit = event => {
+    event.preventDefault()
+    const { name, startsAt, endsAt } = this.state
+
+    this.addItem({
+      project: { name },
+      startsAt,
+      endsAt,
+      id: Math.floor(Math.random() * 100000),
+    })
+    this.setState({ name: "" })
+  }
+
+  render() {
+    const { name, startsAt, endsAt, items } = this.state
+
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <span>Name</span>
+            <input onChange={this.handleChange} name="name" value={name} />
+          </label>
+
+          <label>
+            <span>Starts at</span>
+            <input
+              onChange={this.handleChange}
+              name="startsAt"
+              value={startsAt}
+            />
+          </label>
+
+          <label>
+            <span>Ends at</span>
+            <input onChange={this.handleChange} name="endsAt" value={endsAt} />
+          </label>
+
+          <input type="submit" value="Add project" />
+        </form>
+
+        <Gantt
+          renderItem={(item, options) => (
+            <Row key={item.id} {...item} options={options} />
+          )}
+          items={items}
+          startsAt="2018-01-01"
+          endsAt="2019-12-31"
+        />
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React, { Component } from "react"
+import { differenceInDays } from "date-fns"
 
-import styles from './styles.css'
+import styles from "./styles.css"
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
+export default class Gantt extends Component {
+  optionsForItem = item => {
+    const width = differenceInDays(item.endsAt, item.startsAt)
+    const offset = differenceInDays(item.startsAt, this.props.startsAt)
+
+    console.log("width: ", width, ". offset: ", offset)
+    return {
+      gridColumn: `${offset} / span ${width}`,
+    }
   }
 
   render() {
-    const {
-      text
-    } = this.props
+    const { items, renderItem, startsAt, endsAt } = this.props
 
+    const width = differenceInDays(endsAt, startsAt)
+
+    console.log(width, `repeat(${width}, 1fr)`)
     return (
-      <div className={styles.test}>
-        Example Component: {text}
+      <div
+        className={styles.container}
+        style={{ gridTemplateColumns: `repeat(${width}, 1fr)` }}
+      >
+        {items.map(item => renderItem(item, this.optionsForItem(item)))}
       </div>
     )
   }
